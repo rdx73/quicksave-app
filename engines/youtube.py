5096004c197da04542a74c3c8a6f25558333d3c8
rdx73 <rdx73@users.noreply.github.com>
2026-02-13 23:33:25 +0700

    fix: change player tv

--- engines/youtube.py
+++ engines/youtube.py
@@ -24,7 +24,7 @@ def get_yt_info(video_url):
                 'js_runtime': 'node'
             }
         },
-        'js_runtime':['node']
+        'js_runtime':['node'],
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
